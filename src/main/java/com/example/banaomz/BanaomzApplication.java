38293275d61fceaa4468fc518cdef7f62a812c85
Trần Thị Thanh Thảo <thaotttph36884@fpt.edu.vn>
2025-04-14 21:18:24 +0700

    Push full IntelliJ project

---
+++ src/main/java/com/example/banaomz/BanaomzApplication.java
@@ -0,0 +1,13 @@
+package com.example.banaomz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanaomzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanaomzApplication.class, args);
+    }
+
+}
