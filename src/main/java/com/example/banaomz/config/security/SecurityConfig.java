ce340296c0426a5ce964125a083440a61e8efb81
HoangBadBoy <hoang290124@gmail.com>
2025-09-21 21:26:20 +0700

    Update bán hàng tại quầy

--- src/main/java/com/example/banaomz/config/security/SecurityConfig.java
+++ src/main/java/com/example/banaomz/config/security/SecurityConfig.java
@@ -39,11 +39,12 @@ public class SecurityConfig {
                         "/admin/category/update",// chỉ endpoint này
                         "/admin/customer/address",
 
-                        "auth/forgot-password"
+                        "auth/forgot-password",
 
                         "/auth/forgot-password",
                         "khachhang/dangnhap",
                         "/khachhang/forgot-password",
+                        "/admin/employee/create"
 
                 ))
 
